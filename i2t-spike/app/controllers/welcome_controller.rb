6b7f7ad02d629b8126fa7a503f1fad4a813197f9
Michael Milewski <saramic@gmail.com>
2018-09-13 17:12:15 +1000

    add some more terms and get basic game play working

--- i2t-spike/app/controllers/welcome_controller.rb
+++ i2t-spike/app/controllers/welcome_controller.rb
@@ -1,7 +1,18 @@
 class WelcomeController < ApplicationController
   def index
+    @seed = params.has_key?(:seed) ? params[:seed].to_i : Random.new_seed
     game_images = YAML.load(File.open('config/initializers/game_images.yaml'))
-    @term = game_images['terms'].keys.sample
+    rng = Random.new(@seed)
+    @term = game_images['terms'].keys.sample(1, random: rng).first
+    if params[:term] == @term
+      flash.clear
+      flash[:success] = "#{params[:term].inspect} is correct"
+      @seed = Random.new_seed
+      @term = game_images['terms'].keys.sample(1, random: rng).first
+    else
+      flash.clear
+      flash[:warn] = "#{params[:term].inspect} is NOT correct try again"
+    end
     @images = game_images['terms'][@term]
   end
 end
