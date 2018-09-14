168e1b07f18c765f689efac8b2d2910967101627
Michael Milewski <saramic@gmail.com>
2018-09-15 01:37:08 +1000

    make somewhat playable with pre screens, terms not repeating, win screen

--- i2t-spike/config/routes.rb
+++ i2t-spike/config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/game', to: 'welcome#game', as: 'game'
+  get '/game/round/:id', to: 'welcome#round', as: 'round'
+  get '/game/win', to: 'welcome#win', as: 'win'
+
   root 'welcome#index'
 end
