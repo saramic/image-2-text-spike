66e138857d6ce1a6ba1a847dbf3930b8d430a810
Michael Milewski <saramic@gmail.com>
2018-09-15 17:31:05 +1000

    added devise
    `rails generate devise:install`

--- i2t-spike/config/routes.rb
+++ i2t-spike/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/game(/:id)', to: 'game#game', as: 'game'
