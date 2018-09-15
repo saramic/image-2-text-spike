5270bf5036f727e85a8eb06e29db06a6196a3a54
Michael Milewski <saramic@gmail.com>
2018-09-15 15:40:57 +1000

    moved game and round logic down to a Game model

--- i2t-spike/app/controllers/game_controller.rb
+++ i2t-spike/app/controllers/game_controller.rb
@@ -1,24 +1,21 @@
 class GameController < ApplicationController
-  before_action :read_games
-
   def game
+    game = Game.new
     @round = params.has_key?(:round) ? params[:round].to_i : 1
-    @rounds = params.has_key?(:rounds) ? params[:rounds].to_i : @@game_images['terms'].keys.count
-    @seed = Random.new_seed
+    @rounds = params.has_key?(:rounds) ? params[:rounds].to_i : game.term_count
+    @seed = game.seed
   end
 
   def round
     @seed = params[:seed].to_i
     @round = params[:round].to_i
     @rounds = params[:rounds].to_i
-    rng = Random.new(@seed)
-    @term = @@game_images['terms'].keys.sort_by{ rng }.first(@rounds)[@round - 1]
+    game = Game.new(seed: @seed, round: @round)
     if params[:term]
-      if params[:term] == @term
+      if params[:term] == game.term
         flash.clear
         flash[:success] = "#{params[:term].inspect} is correct"
         @round = @round + 1
-        @term = @@game_images['terms'].keys.sample(1, random: rng).first
         if @round > @rounds
           redirect_to win_path
         else
@@ -29,12 +26,6 @@ class GameController < ApplicationController
         flash[:warn] = "#{params[:term].inspect} is NOT correct try again"
       end
     end
-    @images = @@game_images['terms'][@term]
-  end
-
-  private
-
-  def read_games
-    @@game_images ||= YAML.load(File.open('config/initializers/game_images.yaml'))
+    @images = game.images
   end
 end
---
+++ i2t-spike/app/models/game.rb
@@ -0,0 +1,21 @@
+class Game
+  include ActiveModel::Model
+
+  attr_reader :seed, :round, :term
+
+  def initialize(seed: Random.new_seed, round: 1)
+    @seed = seed
+    @round = round
+    @@game_images ||= YAML.load(File.open('config/initializers/game_images.yaml'))
+    rng = Random.new(@seed)
+    @term = @@game_images['terms'].keys.sort_by{ rng }.first(term_count)[@round - 1]
+  end
+
+  def term_count
+    @@game_images['terms'].keys.count
+  end
+
+  def images
+    @@game_images['terms'][@term]
+  end
+end
