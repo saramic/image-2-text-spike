a624ba1bf91db7962e6e14af0338854391aa2dae
Michael Milewski <saramic@gmail.com>
2018-10-06 18:07:48 +1000

    call the api and render something

--- i2t-spike/client/src/pages/Home.js
+++ i2t-spike/client/src/pages/Home.js
@@ -2,27 +2,55 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
 class Home extends Component {
+  constructor() {
+    super()
+    this.state = {}
+    this.getGames = this.getGames.bind(this)
+  }
+
   componentDidMount() {
+    this.getGames()
+  }
+
+  fetch(endpoint) {
     // TODO a bit of a hack?
     const url = process.env.NODE_ENV === "development" ? 'http://localhost:3001' : ''
-    window.fetch(`${url}/api/games`)
+    return window.fetch(`${url}${endpoint}`)
       .then(response => response.json())
-      .then(json => console.log(json))
       .catch(error => console.log(error))
+  }
+  getGames() {
+    this.fetch('/api/games')
+      .then(games => {
+        if (games.length) {
+          this.setState({games: games})
+        } else {
+          this.setStaet({games: []})
+        }
+      })
   }
 
   render() {
-    return (
-      <div>
-        Home
-        <ul>
-          <Link to="/">Home</Link>
-          <Link to="/rjs-game">Game</Link>
-          <a href="/game">Game Rails</a>
-          <a href="/admin">Admin</a>
-        </ul>
-      </div>
-    )
+    let { games } = this.state
+    return games
+      ? <div>
+          Home
+          <ul>
+            <Link to="/">Home</Link>
+            <Link to="/rjs-game">Game</Link>
+            <a href="/game">Game Rails</a>
+            <a href="/admin">Admin</a>
+          </ul>
+          { games && games.length
+            ? <ul>
+              { Object.keys(games).map((key) => {
+                return <li key={games[key].id}>{games[key].seed}</li>
+              })}
+              </ul>
+            : <p>no games</p>
+          }
+        </div>
+      : <div>loading</div>
   }
 }
 
